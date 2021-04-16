47284d142b1063e871961916870545bf1b58e685
Moaz Elshebly <moazelshebly@outlook.com>
2021-04-16 16:53:00 +0200

    Migrate to null safety

--- lib/flutter_chips_selector.dart
+++ lib/flutter_chips_selector.dart
@@ -5,18 +5,18 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-typedef ChipsBuilder<T> = Widget Function(BuildContext context, ChipsSelectorState<T> state, T data);
+typedef ChipsBuilder<T> = Widget Function(BuildContext context, ChipsSelectorState<T?> state, T? data);
 typedef ChipsInputSuggestions<T> = FutureOr<List<T>> Function(String query);
 typedef ParsedItems<T> = FutureOr<List<T>> Function(String query);
 
 class ChipsSelector<T> extends StatefulWidget {
   ChipsSelector(
-      {Key key,
+      {Key? key,
       this.initialValue = const [],
-      @required this.chipBuilder,
-      @required this.suggestionBuilder,
-      @required this.findSuggestions,
-      @required this.onChanged,
+      required this.chipBuilder,
+      required this.suggestionBuilder,
+      required this.findSuggestions,
+      required this.onChanged,
       this.parseOnLeaving,
       this.decoration,
       this.style,
@@ -24,8 +24,8 @@ class ChipsSelector<T> extends StatefulWidget {
       this.keyboardBrightness = Brightness.light,
       this.textInputType = TextInputType.text,
       this.textInputAction = TextInputAction.done,
-      FocusNode currentFocus,
-      FocusNode nextFocus})
+      FocusNode? currentFocus,
+      FocusNode? nextFocus})
       : this.current = currentFocus ?? FocusNode(),
         this.next = nextFocus ?? FocusNode(),
         super(key: key);
@@ -35,28 +35,28 @@ class ChipsSelector<T> extends StatefulWidget {
   final ChipsInputSuggestions findSuggestions;
   final List<T> initialValue;
   final ValueChanged<List<T>> onChanged;
-  final ParsedItems parseOnLeaving;
-  final InputDecoration decoration;
-  final TextStyle style;
-  final TextInputType textInputType;
-  final TextInputAction textInputAction;
+  final ParsedItems? parseOnLeaving;
+  final InputDecoration? decoration;
+  final TextStyle? style;
+  final TextInputType? textInputType;
+  final TextInputAction? textInputAction;
   final FocusNode current;
   final FocusNode next;
-  final bool autofocus;
+  final bool? autofocus;
   final Brightness keyboardBrightness;
 
   @override
   State<StatefulWidget> createState() => ChipsSelectorState<T>();
 }
 
-class ChipsSelectorState<T> extends State<ChipsSelector<T>> {
+class ChipsSelectorState<T> extends State<ChipsSelector<T?>> {
   TextEditingController _controller = TextEditingController();
   GlobalKey editKey = GlobalKey();
-  OverlayEntry _overlayEntry;
+  late OverlayEntry _overlayEntry;
   final LayerLink _layerLink = LayerLink();
-  Timer searchOnStoppedTyping;
+  Timer? searchOnStoppedTyping;
 
-  List<T> _items = [];
+  List<T?> _items = [];
   List<T> _suggestions = [];
 
   int _selectedIndex = -1;
@@ -74,18 +74,14 @@ class ChipsSelectorState<T> extends State<ChipsSelector<T>> {
     widget.current.addListener(() {
       if (widget.current.hasFocus) {
         this._overlayEntry = this._createOverlayEntry();
-        if (context != null) {
-          Overlay.of(context).insert(this._overlayEntry);
-        }
+        Overlay.of(context)!.insert(this._overlayEntry);
       } else {
-        if (this.context != null) {
-          this._overlayEntry.remove();
-          if (widget.parseOnLeaving != null) {
-            List<T> parsedEntries = widget.parseOnLeaving(_controller.text);
-            parsedEntries.forEach((element) {
-              selectSuggestion(element);
-            });
-          }
+        this._overlayEntry.remove();
+        if (widget.parseOnLeaving != null) {
+          List<T> parsedEntries = widget.parseOnLeaving!(_controller.text) as List<T>;
+          parsedEntries.forEach((element) {
+            selectSuggestion(element);
+          });
         }
       }
     });
@@ -203,12 +199,12 @@ class ChipsSelectorState<T> extends State<ChipsSelector<T>> {
               //wait some time after user has stopped typing
               const duration = Duration(milliseconds: 100);
               if (searchOnStoppedTyping != null) {
-                setState(() => searchOnStoppedTyping.cancel()); // clear timer
+                setState(() => searchOnStoppedTyping!.cancel()); // clear timer
               }
               setState(
                 () => searchOnStoppedTyping = new Timer(duration, () async {
                   if (newText.length > 1) {
-                    _suggestions = await widget.findSuggestions(newText);
+                    _suggestions = await (widget.findSuggestions(newText) as FutureOr<List<T>>);
                   } else {
                     _suggestions.clear();
                   }
@@ -227,8 +223,8 @@ class ChipsSelectorState<T> extends State<ChipsSelector<T>> {
             maxLines: 1,
             autofocus: widget.autofocus ?? true,
             forceLine: false,
-            style: widget.style ?? Theme.of(context).textTheme.bodyText2,
-            cursorColor: Theme.of(context).textSelectionTheme.cursorColor,
+            style: widget.style ?? Theme.of(context).textTheme.bodyText2!,
+            cursorColor: Theme.of(context).textSelectionTheme.cursorColor!,
             backgroundCursorColor: Theme.of(context).backgroundColor,
             focusNode: widget.current,
             controller: _controller,
@@ -239,12 +235,12 @@ class ChipsSelectorState<T> extends State<ChipsSelector<T>> {
   }
 
   OverlayEntry _createOverlayEntry() {
-    RenderBox renderBox = context?.findRenderObject();
+    RenderBox? renderBox = context.findRenderObject() as RenderBox?;
     var size = renderBox?.size;
 
     return OverlayEntry(
       builder: (context) => Positioned(
-        width: context != null ? size.width : 20.0,
+        width: size!.width,
         child: Visibility(
           visible: _suggestions.isNotEmpty,
           child: CompositedTransformFollower(
