fa7d3732085e518d984c8a59ecb5f288c0f01c6a
ltOgt <omnesiaorg@gmail.com>
2023-03-07 13:49:45 +0100

    add secondary test condition

--- test/flutter_chips_selector_test.dart
+++ test/flutter_chips_selector_test.dart
@@ -17,6 +17,70 @@ Widget _withRequiredParents(Widget child) => MaterialApp(
 
 void main() {
   group('ChipSelector', () {
+    testWidgets("Suggestion Overlay is closed if focus is lost", (tester) async {
+      final GlobalKey<ChipsSelectorState> chipStateKey = GlobalKey();
+      final GlobalKey suggestionKey = GlobalKey();
+
+      final FocusNode otherFocus = FocusNode();
+
+      await tester.pumpWidget(
+        _withRequiredParents(
+          Stack(
+            children: [
+              Focus(
+                focusNode: otherFocus,
+                child: SizedBox(
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+              ChipsSelector<String>(
+                key: chipStateKey,
+                chipBuilder: (context, state, data) => Container(color: Colors.blue, width: 10, height: 10),
+                suggestionBuilder: (context, state, data) => Container(
+                  key: suggestionKey,
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                findSuggestions: (query) {
+                  return ["SUGGESTION"];
+                },
+                onChanged: (value) {
+                  print(value);
+                },
+              )
+            ],
+          ),
+        ),
+      );
+
+      /// Get chip state to access other fields
+      final chipState = chipStateKey.currentState;
+      expect(chipState, isNotNull);
+      chipState!;
+
+      /// Get text field and enter some text to trigger a suggestion
+      final textFieldWidget = find.byKey(chipState.editKey);
+      expect(textFieldWidget, findsOneWidget);
+      await tester.enterText(textFieldWidget, "SUG");
+      await tester.pumpAndSettle();
+
+      /// Check if the suggestion widget is shown
+      final suggestionWidget = find.byKey(suggestionKey);
+      expect(suggestionWidget, findsOneWidget);
+
+      /// Check that the overlay is visible
+      expect(chipState.overlayEntry.mounted, isTrue);
+
+      /// Remove focus
+      otherFocus.requestFocus();
+      await tester.pumpAndSettle();
+
+      /// Check that the overlay is no longer visible
+      expect(chipState.overlayEntry.mounted, isFalse);
+    });
+
     testWidgets("Suggestion Builder onTap callbacks are executed", (tester) async {
       int onTapCounter = 0;
       final GlobalKey buttonKey = GlobalKey();
@@ -75,11 +139,11 @@ void main() {
       await gesture.up();
       await tester.pump();
 
-      /// Check that the overlay is no longer visible
-      expect(chipState.overlayEntry.mounted, isFalse);
-
       /// Check that the suggestion widgets callback has been called
       expect(onTapCounter, 1);
+
+      /// Check that the overlay is no longer visible
+      expect(chipState.overlayEntry.mounted, isFalse);
     });
   });
 }
