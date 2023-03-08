948a7603882bf5e1796fc8cf98f4000b04da3f35
ltOgt <omnesiaorg@gmail.com>
2023-03-08 14:02:48 +0100

    clean up example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,16 +27,9 @@ class _MyApp extends StatelessWidget {
 class MyWidget extends StatelessWidget {
   const MyWidget({Key? key}) : super(key: key);
 
-  static final FocusNode fn = FocusNode(debugLabel: "textFocus");
-  static final FocusNode fnTile = FocusNode(debugLabel: "tile");
-
-  static final GlobalKey<ChipsSelectorState> _key = GlobalKey();
-
   @override
   Widget build(BuildContext context) {
     return ChipsSelector<String>(
-      key: _key,
-      currentFocus: fn,
       underlineColor: Colors.black,
       chipBuilder: (context, state, data) => Container(
         decoration: BoxDecoration(
@@ -55,13 +48,9 @@ class MyWidget extends StatelessWidget {
       ),
       autofocus: true,
       suggestionBuilder: (context, state, data) {
-        // TODO would like to know here whether data is selected or not
-        //  add state.isSelected(data) and return null;
         return ListTile(
-          focusNode: fnTile,
-          key: ValueKey(data),
           onTap: () {
-            _key.currentState!.selectSuggestion(data);
+            state.selectSuggestion(data);
           },
           title: Container(
             decoration: BoxDecoration(
@@ -72,19 +61,17 @@ class MyWidget extends StatelessWidget {
           ),
         );
       },
-      findSuggestions: (query) {
-        switch (query.toLowerCase()) {
-          case "hello":
-            return ["hello world", "hello you"];
-          case "ping":
-            return ["ping pong"];
-          case "marco":
-            return ["marco polo"];
-          default:
-            return ["default suggestion #1", "default suggestion #2"];
-        }
+      findSuggestions: (query) => [
+        "Chip Option 1",
+        "Chip Option 2",
+        "Chip Option 3",
+        "Chip Option 4",
+        "Chip Option 5",
+        "Chip Option 6",
+      ],
+      onChanged: (v) {
+        // use the updated list of chips here
       },
-      onChanged: (v) {},
     );
   }
 }
